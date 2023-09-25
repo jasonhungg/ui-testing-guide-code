5b97843d9677cdfb929692cda862ce1ea9104fe9
Jason Hung <jasonhung@pisolutions.tech>
2023-09-25 14:27:21 +0800

    add msw addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,15 @@
-import "../src/index.css";
+import '../src/index.css';
+
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [mswDecorator],
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
